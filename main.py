dab1e28636a79f6db1e4499ff69add29e52e5bdc
DrRawley <rawley.greene@gmail.com>
2024-05-03 09:09:16 -0400

    day 3

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-user_prompt = "Enter a \"todo\": "
 todos = []
 
 while True:
-  todo = input(user_prompt)
-  todos.append(todo)
-  print(todos)
+  user_action = input("Type add, show, or exit: ")
+  match user_action.strip():  # .strip() removes trailing spaces
+    case "add":
+      todo = input("Enter a todo: ")
+      todos.append(todo.strip())
+      pass
+    case "show" | "display":
+      #print(todos)
+      for item in todos:
+        print(item.title())
+      pass
+    case "exit":
+      exit(0)
+    case _:
+      print("Not a valid command.")
 
 
